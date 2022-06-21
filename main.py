9a6496caab59cc4404684bf08a18acfb9043eac8
Shamsur <shamsur314@gmail.com>
2022-06-21 08:21:03 +0000

    request to richup io api testing

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import discord 
+import discord
+import os
+import requests
+
+x = requests.get('https://richup.io/api/room/new?isPrivate=true')
+print(x)
+
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     print(f'We have logged in as {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+
+#     if message.content == '/sendLink':
+#         res = "link"
+#         await message.channel.send(res)
+
+# client.run(os.getenv('TOKEN'))
