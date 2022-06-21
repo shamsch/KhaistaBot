3c58e4c9c90d786ed724ce88492de78ec480a650
Shamsur <shamsur314@gmail.com>
2022-06-21 08:40:30 +0000

    room id from get request

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import discord
 import os
 import requests
+from keep_alive import keep_alive
+import json 
 
-x = requests.get('https://richup.io/api/room/new?isPrivate=true')
-print(x)
+res = requests.get('https://richup.io/api/room/new?isPrivate=true')
+res= res.json()
+roomId = res['roomId']
 
-# client = discord.Client()
+client = discord.Client()
 
-# @client.event
-# async def on_ready():
-#     print(f'We have logged in as {client.user}')
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-#     if message.content == '/sendLink':
-#         res = "link"
-#         await message.channel.send(res)
+    if message.content == '/sendLink':
+        res = "link"
+        await message.channel.send(res)
+    
+      #skar khobis, khaista 
+    if message.content == '/whoIsKhaista': 
+        res= "Skar ওরফে Shiblu!"
 
+# keep_alive()
 # client.run(os.getenv('TOKEN'))
